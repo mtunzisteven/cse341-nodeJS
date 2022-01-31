81a4c834fc6e36c4c59380613d6ef6094ec7de30
mtunzisteven <mtunzism@byui.edu>
2022-01-31 06:17:10 +0200

    store

---
+++ models/order.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose'); // import mongoose
+const Schema = mongoose.Schema; // store schema 
+
+const orderSchema = new Schema({
+
+  products:
+    [
+      {
+        product: {type: Object, required: true},
+        quantity: {type: Number, required: true}
+      }
+    ],
+    user: {
+      name: {
+        type: String,
+        required: true
+      },
+      userId: {
+        type: Schema.Types.ObjectId, 
+        ref: 'User', 
+        required: true
+      }
+    }
+});
+
+// order arg is a name we give to our exported schema | Caps first letter
+// this same name is used to create a collection in db | order creates orders, Cat creates cats
+module.exports = mongoose.model('Order', orderSchema); // this export connects the model with the schema
