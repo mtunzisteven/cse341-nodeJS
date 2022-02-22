f5a17dcabc271cdef57e7561839835d489164687
mtunzisteven <mtunzism@byui.edu>
2022-02-22 05:43:14 +0200

    API section

--- apiSection/routes/feed.js
+++ apiSection/routes/feed.js
@@ -7,7 +7,7 @@ const router = express.Router();
 //GET /feed/posts
 router.get('/posts', feedController.getPosts);
 
-//POST /feed/posts
+//POST /feed/post
 router.post('/post', feedController.postPost);
 
 module.exports = router;
